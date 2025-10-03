76c83423370239b9db3d575112e1d4adf21fd196
Kevin Galvan <71290362+bevins93@users.noreply.github.com>
2025-10-03 10:35:18 -0700

    Initial commit - cookiecutter data science structure

---
+++ arhbpalr/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Este proyecto utiliza técnicas de ciencia de datos e ingeniería de características para evaluar la importancia actual de la presa Abelardo L. Rodríguez en Hermosillo, Sonora, como un sistema socio-ecológico vital. A través del análisis de datos hidrológicos, climáticos y de biodiversidad, buscamos cuantificar su rol en la recarga de acuíferos y como soporte de un ecosistema clave, con un enfoque especial en la avifauna.',
+    author='MCDproyectos',
+    license='MIT',
+)
